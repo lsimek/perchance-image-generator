524c91cc1cc3bc5e20930642a29670e1685eaafc
raymerjacque <raymerjacque@gmail.com>
2024-03-05 14:49:18 +0700

    Delete wordlist.py
    no longer needed

--- wordlist.py
+++
@@ -1,9 +0,0 @@
-from random import sample
-
-
-class MyWordlist:
-    def __init__(self):
-        self.wordlist = ["Dragon", "Castle", "Moon", "Forest", "Apple", "Fish", "Green", "Banana", "Queen Elizabeth I", "Inspiration"]
-
-    def get_prompt(self, number=10):  # returns 'first, second, third...' type string
-        return ', '.join(sample(self.wordlist, number))
